2fffd943a92e02002880eb331307506891ac2931
CTRedstone <felixlangefelixlange@gmail.com>
2026-02-11 07:26:06 +0100

    Update Makefile.mk
    Disable protoc plugin for OLAD, original file this is a patch for is ola/protoc/Makefile.mk

--- package/olad/patches/Makefile.mk
+++ package/olad/patches/Makefile.mk
@@ -1,22 +1,8 @@
 # Programs
 #########################
 if BUILD_OLA_PROTOC_PLUGIN
-noinst_PROGRAMS += protoc/ola_protoc_plugin
-
-protoc_ola_protoc_plugin_SOURCES = \
-    protoc/CppFileGenerator.cpp \
-    protoc/CppFileGenerator.h \
-    protoc/CppGenerator.cpp \
-    protoc/CppGenerator.h \
-    protoc/GeneratorHelpers.cpp \
-    protoc/GeneratorHelpers.h \
-    protoc/ServiceGenerator.cpp \
-    protoc/ServiceGenerator.h \
-    protoc/StrUtil.cpp \
-    protoc/StrUtil.h \
-    protoc/ola-protoc-generator-plugin.cpp
-protoc_ola_protoc_plugin_CXXFLAGS = $(COMMON_PROTOBUF_CXXFLAGS)
-protoc_ola_protoc_plugin_LDADD = $(libprotobuf_LIBS) -lprotoc
+# Plugin disabled
+noinst_PROGRAMS =
 
 else
 
