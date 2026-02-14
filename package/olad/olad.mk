c216a23e1472fc326f86c5a13ffde9b45a91f7a3
CTRedstone <felixlangefelixlange@gmail.com>
2026-02-14 14:22:30 +0100

    Update olad.mk
    Removed hook copy due to older version of protobuf in use right now

--- package/olad/olad.mk
+++ package/olad/olad.mk
@@ -8,21 +8,22 @@ OLAD_CONF_OPTS = --disable-fatal-warnings
 OLAD_CONF_OPTS += --without-ola-protoc-plugin
 OLAD_CONF_OPTS += --disable-examples
 
-define OLAD_COPY_MISSING_RPC_FILES
-    cp $(OLAD_PKGDIR)/files/common/rpc/TestService.pb.* $(@D)/common/rpc/
-endef
+# The following lines aren't needed anyymore due to change of used protobuf version
+#define OLAD_COPY_MISSING_RPC_FILES
+#    cp $(OLAD_PKGDIR)/files/common/rpc/TestService.pb.* $(@D)/common/rpc/
+#endef
 
-define OLAD_COPY_MISSING_RPC_FILES
-	cp $(OLAD_PKGDIR)/files/common/rpc/TestService.pb.* $(@D)/common/rpc/
-endef
+#define OLAD_COPY_MISSING_RPC_FILES
+#	cp $(OLAD_PKGDIR)/files/common/rpc/TestService.pb.* $(@D)/common/rpc/
+#endef
 
 define OLAD_COPY_MISSING_RDM_FILES
-	cp $(OLAD_PKGDIR)/files/common/rdm/Pids.pb.* $(@D)/common/rdm/
-endef
+#	cp $(OLAD_PKGDIR)/files/common/rdm/Pids.pb.* $(@D)/common/rdm/
+#endef
 
-OLAD_POST_PATCH_HOOKS += OLAD_COPY_MISSING_RPC_FILES
-OLAD_POST_PATCH_HOOKS += OLAD_COPY_MISSING_RDM_FILES
-OLAD_POST_PATCH_HOOKS += OLAD_COPY_MISSING_RPC_FILES
+#OLAD_POST_PATCH_HOOKS += OLAD_COPY_MISSING_RPC_FILES
+#OLAD_POST_PATCH_HOOKS += OLAD_COPY_MISSING_RDM_FILES
+#OLAD_POST_PATCH_HOOKS += OLAD_COPY_MISSING_RPC_FILES
 
 OLAD_AUTORECONF = YES
 
