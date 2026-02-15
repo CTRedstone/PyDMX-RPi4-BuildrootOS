be9e6a58c26ef879217f6b7a02113b2d8f54a3b5
CTRedstone <felixlangefelixlange@gmail.com>
2026-02-15 12:41:29 +0100

    Update olad.mk

--- package/olad/olad.mk
+++ package/olad/olad.mk
@@ -5,8 +5,31 @@ OLAD_SOURCE = ola-$(OLAD_VERSION).tar.gz
 OLAD_DEPENDENCIES = protobuf libmicrohttpd
 
 OLAD_CONF_OPTS = --disable-fatal-warnings
-#OLAD_CONF_OPTS += --without-ola-protoc-plugin
+OLAD_CONF_OPTS += --without-ola-protoc-plugin
 OLAD_CONF_OPTS += --disable-examples
+
+OLAD_AUTORECONF = YES
+
+define OLAD_FIX_PLUGIN_PATH
+	mkdir -p $(@D)/protoc
+	cp $(HOST_DIR)/bin/ola_protoc_plugin $(@D)/protoc/ola_protoc_plugin
+endef
+
+OLAD_POST_PATCH_HOOKS += OLAD_FIX_PLUGIN_PATH
+
+$(eval $(autotools-package))
+
+# COMMENTED FOR NEW VERSION YOU SEE ABOVE THIS
+
+#OLAD_VERSION = 0.10.8
+#OLAD_SITE = https://github.com/OpenLightingProject/ola/releases/download/$(OLAD_VERSION)
+#OLAD_SOURCE = ola-$(OLAD_VERSION).tar.gz
+
+#OLAD_DEPENDENCIES = protobuf libmicrohttpd
+
+#OLAD_CONF_OPTS = --disable-fatal-warnings
+#OLAD_CONF_OPTS += --without-ola-protoc-plugin
+#OLAD_CONF_OPTS += --disable-examples
 
 # The following lines aren't needed anyymore due to change of used protobuf version
 #define OLAD_COPY_MISSING_RPC_FILES
@@ -25,37 +48,37 @@ OLAD_CONF_OPTS += --disable-examples
 #OLAD_POST_PATCH_HOOKS += OLAD_COPY_MISSING_RDM_FILES
 #OLAD_POST_PATCH_HOOKS += OLAD_COPY_MISSING_RPC_FILES
 
-OLAD_AUTORECONF = YES
+#OLAD_AUTORECONF = YES
 #HOST_OLAD_DEPENDENCIES = host-protobuf
 #HOST_OLAD_CONF_OPTS = --disable-shared --enable-static
 #HOST_OLAD_AUTORECONF = YES
 
-OLAD_CONF_OPTS += --without-ola-protoc-plugin
+#OLAD_CONF_OPTS += --without-ola-protoc-plugin
 #HOST_OLAD_CONF_OPTS += --with-ola-protoc-plugin
-define OLAD_FIX_PLUGIN_PATH
-	mkdir -p $(@D)/protoc
-	cp $(HOST_DIR)/bin/ola_protoc_plugin $(@D)/protoc/ola_protoc_plugin
-endef
+#define OLAD_FIX_PLUGIN_PATH
+#	mkdir -p $(@D)/protoc
+#	cp $(HOST_DIR)/bin/ola_protoc_plugin $(@D)/protoc/ola_protoc_plugin
+#endef
 
-OLAD_POST_PATCH_HOOKS += OLAD_FIX_PLUGIN_PATH
+#OLAD_POST_PATCH_HOOKS += OLAD_FIX_PLUGIN_PATH
 
-$(eval $(autotools-package))
+#$(eval $(autotools-package))
 
 # VVV In case that target architecture isn't the same as the build process is executed on VVV
 # Configure host package
 
-HOST_OLAD_VERSION = $(OLAD_VERSION)
-HOST_OLAD_SOURCE = $(OLAD_SOURCE)
-HOST_OLAD_SITE = $(OLAD_SITE)
+#HOST_OLAD_VERSION = $(OLAD_VERSION)
+#HOST_OLAD_SOURCE = $(OLAD_SOURCE)
+#HOST_OLAD_SITE = $(OLAD_SITE)
 
-HOST_OLAD_DEPENDENCIES = host-protobuf
-HOST_OLAD_AUTORECONF = YES
+#HOST_OLAD_DEPENDENCIES = host-protobuf
+#HOST_OLAD_AUTORECONF = YES
 
-HOST_OLAD_CONF_OPTS = \
-	--disable-shared \
-	--enable-static \
-	--without-ola-protoc-plugin
+#HOST_OLAD_CONF_OPTS = \
+#	--disable-shared \
+#	--enable-static \
+#	--without-ola-protoc-plugin
 
-$(eval $(host-autotools-package))
+#$(eval $(host-autotools-package))
 
 #$(eval $(host-autotools-package))
